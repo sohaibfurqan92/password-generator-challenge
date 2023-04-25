32f05185c7142482a0aef512e6aa3326a6e269a0
sohaibfurqan92 <sohaibfurqan@hotmail.com>
2023-04-25 22:20:52 +0100

    Fixing mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import PasswordGenerator from './components/PasswordGenerator';
 
 function App() {
   return (
-    <div className=" bg-gray-1 h-screen text-white flex flex-col justify-center items-center">
-      <div className="w-1/2 container mx-auto h-full p-5">
+    <div className=" bg-gray-1 h-screen text-white flex flex-col md:justify-center md:items-center">
+      <div className="md:w-1/2 md:container md:mx-auto h-full p-5">
         <h1 className="text-3xl font-bold mb-5 text-center">
           Password Generator
         </h1>
